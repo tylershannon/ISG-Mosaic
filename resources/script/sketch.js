8ac57a29194e47c414b4e8fc76033df93b464bda
Tyler Shannon <tylershannon@Tylers-MacBook-Pro.local>
2017-09-20 15:41:03 -0400

    basis sketch

--- resources/script/sketch.js
+++ resources/script/sketch.js
@@ -1,12 +1,15 @@
+//initialize variables
+var portrait;
+
+function preload(){
+  img = loadImage("./resources/images/portrait/portrait_cropped_16_9.jpg")
+}
+
 function setup() {
   createCanvas((windowHeight*9)/16, windowHeight);
+  background('#393f93')
 }
 
 function draw() {
-  if(mouseIsPressed){
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX,mouseY,80,80);
+
 }
