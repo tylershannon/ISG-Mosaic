d395a77879a2cfbfde0bae424990d747013336f8
Tyler Shannon <tylershannon@Tylers-MacBook-Pro.local>
2017-09-20 15:32:39 -0400

    basis sketch

---
+++ resources/script/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas((windowHeight*9)/16, windowHeight);
+}
+
+function draw() {
+  if(mouseIsPressed){
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX,mouseY,80,80);
+}
