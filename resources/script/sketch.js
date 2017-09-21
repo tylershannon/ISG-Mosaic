91d6f05db41cdd0382a880e1b6f760e49d4e50ab
Tyler Shannon <tylershannon@Tylers-MacBook-Pro.local>
2017-09-21 12:48:30 -0400

    basis sketch

--- resources/script/sketch.js
+++ resources/script/sketch.js
@@ -1,15 +1,24 @@
 //initialize variables
 var portrait;
+var pxWidth;
+var pxCount_width;
+var displayWidth
+var displayHeight
 
 function preload(){
   portrait = loadImage("./resources/images/portrait/portrait_cropped_16_9.jpg");
 }
 
 function setup() {
-  createCanvas((windowHeight*9)/16, windowHeight);
+  displayHeight = windowHeight;
+  displayWidth = (windowHeight*9)/16
+  createCanvas((windowHeight*9)/16, displayHeight);
   background('#393f93');
-  image(portrait, 0,0, (windowHeight*9)/16, windowHeight);
-  console.log(1);
+  image(portrait, 0,0, displayWidth, windowHeight);
+  console.log(2);
+  pxCount_width = 80;
+  pxWidth = displayWidth/pxCount_width;
+  console.log(pxWidth);
 
 }
 
